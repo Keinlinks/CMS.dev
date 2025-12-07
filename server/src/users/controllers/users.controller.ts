37cc238cada39c65c53a56b697bb5309ad0b0fcb
Keinlinks <kevin.clift.buzeta@gmail.com>
2025-12-06 21:45:37 -0300

    hotfix:user controller bearer for swagger

--- server/src/users/controllers/users.controller.ts
+++ server/src/users/controllers/users.controller.ts
@@ -13,17 +13,20 @@ import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
 import { GetUser } from 'src/common/decorators/get-user.decorator';
 import { User } from '../entities/user.entity';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('users/me')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @ApiBearerAuth()
   findMe(@GetUser() user:User) {
     return this.usersService.findOneWithOrganizations(user.id);
   }
 
   @Patch()
+  @ApiBearerAuth()
   updateMe(
     @GetUser() user:User,
     @Body() updateUserDto: UpdateUserDto,
