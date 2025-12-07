f23b8f10ec411218150fb91bb30e63e8e8ec0ce2
Keinlinks <kevin.clift.buzeta@gmail.com>
2025-12-06 22:26:48 -0300

    documented user controllers

--- server/src/users/controllers/users.controller.ts
+++ server/src/users/controllers/users.controller.ts
@@ -13,7 +13,7 @@ import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
 import { GetUser } from 'src/common/decorators/get-user.decorator';
 import { User } from '../entities/user.entity';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 
 @Controller('users/me')
 export class UsersController {
@@ -21,12 +21,14 @@ export class UsersController {
 
   @Get()
   @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get user entity of authenticated user' })
   findMe(@GetUser() user:User) {
     return this.usersService.findOneWithOrganizations(user.id);
   }
 
   @Patch()
   @ApiBearerAuth()
+  @ApiOperation({ summary: 'Update authenticated user' })
   updateMe(
     @GetUser() user:User,
     @Body() updateUserDto: UpdateUserDto,
